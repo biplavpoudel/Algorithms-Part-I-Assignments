ccf7b977ac0519e526b375caddd08a752c81182d
biplavpoudel <biplavpoudel764@gmail.com>
2025-04-14 22:15:03 +0545

    tons of fixes needed

--- queue/src/Deque.java
+++ queue/src/Deque.java
@@ -4,6 +4,9 @@
  *  Last modified:     14 Apr, 2025
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -11,18 +14,16 @@ public class Deque<Item> implements Iterable<Item> {
     private Item[] arr;
     private int head;        // let's use circular array with no fixed ends
     private int tail;
-    private int N;
-
 
     // construct an empty deque
-    public Deque() {
-        @SuppressWarnings("unchecked")
-        Item[] arr = (Item[]) new Object[1];
+    @SuppressWarnings("unchecked")
+    public Deque(int N) {
+        arr = (Item[]) new Object[N];
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-        return head == tail && arr[head].equals("null");
+        return head == tail && arr[head] == null;
     }
 
     // return the number of items on the deque
@@ -33,7 +34,7 @@ public class Deque<Item> implements Iterable<Item> {
     public void resize(int capacity) {
         @SuppressWarnings("unchecked")
         Item[] new_arr = (Item[]) new Object[capacity];
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < size(); i++) {
             new_arr[i] = arr[i];
         }
         arr = new_arr;
@@ -41,27 +42,46 @@ public class Deque<Item> implements Iterable<Item> {
 
     // add the item to the front
     public void addFirst(Item item) {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
+        if (item == null) {
+            throw new IllegalArgumentException();
         }
         // if array full, resize first
         if ((tail + 1) % arr.length == head % arr.length) resize(arr.length * 2);
         // then shift head to the left and add new item to it
-        head = (head - 1) % arr.length;
+        head = (head - 1 + arr.length) % arr.length;
         arr[head] = item;
-
     }
 
     // add the item to the back
     public void addLast(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+        // if array full, resize first
+        if ((tail + 1 + arr.length) % arr.length == head % arr.length) resize(arr.length * 2);
+        // then shift head to the left and add new item to it
+        tail = (tail + 1 + arr.length) % arr.length;
+        arr[tail] = item;
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Item item = arr[head];
+        head = (head + 1 + arr.length) % arr.length;
+        return item;
     }
 
     // remove and return the item from the back
     public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Item item = arr[tail];
+        tail = (tail - 1 + arr.length) % arr.length;
+        return item;
     }
 
     // return an iterator over items in order from front to back
@@ -70,17 +90,38 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     public class ArrayIterator implements Iterator<Item> {
+        private int i = 0;
 
         public boolean hasNext() {
-            return false;
+            return i < size();
         }
 
         public Item next() {
-            return null;
+            return arr[i++];
+        }
+
+
+        public void remove() {
+            throw new UnsupportedOperationException();
         }
     }
 
     // unit testing (required)
     public static void main(String[] args) {
+        int N = StdIn.readInt();
+        Deque<Integer> deque = new Deque<>(N);
+        while (!StdIn.isEmpty()) {
+            deque.addLast(StdIn.readInt());
+        }
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(99);
+        deque.addLast(20);
+        int a = deque.removeFirst();
+        int b = deque.removeLast();
+        StdOut.printf("the first and last items deleted are: %d and %d", a, b);
+        for (Integer item : deque.arr) {
+            StdOut.print(item);
+        }
     }
 }
