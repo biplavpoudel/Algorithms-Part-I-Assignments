3daa9f011aff0c3386451bd9d5ea571c6c69ca43
biplavpoudel <biplavpoudel764@gmail.com>
2025-05-10 15:19:59 +0545

    new class created

---
+++ mergeSort/src/FastCollinearPoints.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              Biplav Poudel
+ *  Coursera User ID:  https://www.coursera.org/learner/biplavpoudel
+ *  Last modified:     10 May, 2025
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ mergeSort/src/Point.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              Biplav Poudel
+ *  Coursera User ID:  https://www.coursera.org/learner/biplavpoudel
+ *  Last modified:     10 May, 2025
+ **************************************************************************** */
+
+public class Point {
+    public static void main(String[] args) {
+
+    }
+}
