0fdad82e1cd21d9b82f47974c04b3da74a175790
biplavpoudel <biplavpoudel764@gmail.com>
2025-07-24 23:02:31 +0545

    feature: added function to calculate manhatten distance

--- priorityQueue/src/Board.java
+++ priorityQueue/src/Board.java
@@ -45,14 +45,55 @@ public class Board {
         return this.tiles.length;
     }
 
+    private int[][] getGoal() {
+        int size = dimension();
+        // Goal tile starts with 1
+        int item = 1;
+        int[][] goal = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                goal[i][j] = item++;
+            }
+        }
+        // Last tile (bottom-right) is always empty in goal
+        goal[size - 1][size - 1] = 0;
+        return goal;
+    }
+
     // number of tiles out of place
     public int hamming() {
-        return 0;
+        int[][] goal = getGoal();
+        int size = dimension();
+        int wrongPositionCounter = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                // we ignore position for empty tile
+                if (tiles[i][j] == 0) continue;
+                if (this.tiles[i][j] != goal[i][j]) wrongPositionCounter++;
+            }
+        }
+        return wrongPositionCounter;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        return 0;
+        int size = dimension();
+        int manhattenSum = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                // we ignore position for empty tile
+                if (tiles[i][j] == 0) continue;
+                // -1 in both rows and columns is to account for 0-indexing
+                // i.e. (1,1) tile is stored at (0,0) array
+                // 24 in 5x5 tile is stored at: (23 = 5*4+3) = (4,3) for 0th index
+                int goal_row = (int) Math.floor((double) (this.tiles[i][j] - 1) / size);
+                int goal_col = (int) ((double) (this.tiles[i][j] - 1) % size);
+                int manhattenDist = Math.abs(goal_col - j) + Math.abs(goal_row - i);
+                StdOut.printf("Manhatten distance for %d is: %d\n", tiles[i][j], manhattenDist);
+                manhattenSum += manhattenDist;
+            }
+        }
+        return manhattenSum;
     }
 
     // is this board the goal board?
@@ -93,7 +134,8 @@ public class Board {
         }
         Board board = new Board(tiles);
         StdOut.print(board.toString());
-
+        StdOut.printf("\nThe number of wrongly placed tiles is: %d\n", board.hamming());
+        StdOut.printf("\nThe Manhatten sum is: %d\n", board.manhattan());
 
     }
 }
