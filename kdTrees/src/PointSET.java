6cc3664a48e9a5344cfcdd5ae7b221f9dfe65142
biplavpoudel <biplavpoudel764@gmail.com>
2025-08-07 20:53:54 +0545

    feature: added apis for both classes: PointSET and KdTree

---
+++ kdTrees/src/PointSET.java
@@ -0,0 +1,54 @@
+/* *****************************************************************************
+ *  Name:              Biplav Poudel
+ *  Coursera User ID:  https://www.coursera.org/learner/biplavpoudel
+ *  Last modified:     07 Aug, 2025
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+
+public class PointSET {
+
+    // Constructs an empty set of points
+    public PointSET() {
+    }
+
+    // Returns true if the set is empty
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // Returns the number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // Adds the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+    }
+
+    // Returns true if the set contains point p
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    // Draws all points to standard draw
+    public void draw() {
+    }
+
+    // Returns all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // Returns a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+
+    // Unit testing of the methods
+    public static void main(String[] args) {
+    }
+}
+
