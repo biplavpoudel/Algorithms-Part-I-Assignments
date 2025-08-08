86fdb68293d760d667f49e490cb44e83b080ae68
biplavpoudel <biplavpoudel764@gmail.com>
2025-08-08 21:47:27 +0545

    feature: added input files for testing

--- kdTrees/src/PointSET.java
+++ kdTrees/src/PointSET.java
@@ -6,26 +6,37 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+// A data type to represent a set of points in the unit square
+// (all points have x- and y-coordinates between 0 and 1)
+// using a 2d-tree to support efficient range search (find all the points contained in a query rectangle)
+// and nearest-neighbor search (find the closest point to a query point).
 
 
 public class PointSET {
 
+    private SET<Point2D> points;
+
     // Constructs an empty set of points
     public PointSET() {
+        this.points = new SET<>();
     }
 
     // Returns true if the set is empty
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     // Returns the number of points in the set
     public int size() {
-        return 0;
+        return this.points.size();
     }
 
     // Adds the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (this.points.contains(p)) return;
+        this.points.add(p);
     }
 
     // Returns true if the set contains point p
@@ -49,6 +60,7 @@ public class PointSET {
 
     // Unit testing of the methods
     public static void main(String[] args) {
+
     }
 }
 
